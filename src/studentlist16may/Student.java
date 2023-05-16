e1623d53e4af0e175c4ee4a6643aa996eb0d6e17
Ishika Mahajan <mahajish@sheridancollege.ca>
2023-05-16 19:29:29 +0530

    First Commit

---
+++ src/studentlist16may/Student.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist16may;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Student {
+    private String sname;
+    private String sId;
+
+   
+    public Student(String sname, String sId) {
+        this.sname = sname;
+        this.sId = sId;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getsId() {
+        return sId;
+    }
+
+    public void setsId(String sId) {
+        this.sId = sId;
+    
+}
+}
---
+++ src/studentlist16may/StudentList16May.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentlist16may;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.Scanner;
+public class StudentList16May {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Student s1 = new Student("Ishika","s1");
+      
+        
+            
+    }
+    
+        // TODO code application logic here
+    }
+    
+
